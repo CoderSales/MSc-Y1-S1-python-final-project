b9743505f6ccad4d1da5854c884c1d8162e2d5c8
Coder731 <stephenobrien1@gmail.com>
2023-11-21 23:51:26 +0000

    Mid coding empty lists into object
    Add source texts
    for 5
    languages
    
    README
    Add References
    for source
    texts

--- README.md
+++ README.md
@@ -34,6 +34,28 @@ Google Search String: dolch list | [Dolch Word List – 220 High Frequency Sight
 
 [1000 MOST COMMON ITALIAN WORDS](https://1000mostcommonwords.com/1000-most-common-italian-words/)
 
+### Source Texts (Wikipedia) [accessed Tuesday 2023-11-21-at-1421hours]
+
+#### English
+
+sourceEnglish.txt | [Wikipedia](https://en.wikipedia.org/wiki/Wikipedia)
+
+#### French
+
+sourceFrench.txt | [Wikipédia](https://fr.wikipedia.org/wiki/Wikip%C3%A9dia)
+
+#### German
+
+sourceGerman.txt | [Wikipedia](https://de.wikipedia.org/wiki/Wikipedia)
+
+#### Spanish
+
+sourceSpanish.txt | [Wikipedia](https://es.wikipedia.org/wiki/Wikipedia)
+
+#### Italian
+
+sourceItalian | [Wikipedia](https://it.wikipedia.org/wiki/Wikipedia)
+
 #### Approach
 
 Google Search: how to do language detection basic | [How can we detect a language?](https://towardsdatascience.com/how-to-do-language-detection-using-python-nltk-and-some-easy-statistics-6cec9a02148)
--- main.py
+++ main.py
@@ -1,11 +1,81 @@
-languages = ['English.txt', 'French.txt', 'German.txt', 'Spanish.txt', 'Italian.txt']
+"""
+- This project looks at 5 Latin character languages: English, French, German, Spanish, Italian
+- 2 lists: 1. list of Dolch words, 2. list of source texts
+- Each list contains 5 different languages
+- have empty list of words for the 5 languages
+- each empty list is paired with a count of words found in that language
 
-listEnglish = []
-countEnglish = 0
+- have the same for loop structure which occurs twice (outer loop and inner loop):
+- first for each of the source texts
+- then for each of the Dolch word lists
+- then finally it does a comparison
 
-for language in languages: 
-    file = open(language,"r")
-        for line in file:
-            if word in line:
+- TODO:
+- if found in languageDolch: (i.e. potential language match) :
+- append to list      (for all languages in which the word is found in the Dolch word list)
+- AND increment count (for all languages in which the word is found in the Dolch word list)
+
+- Finally:
+- which ever is the highest count
+- this is the language detected.
+
+
+"""
+
+languageDolchs = ['English.txt', 'French.txt', 'German.txt', 'Spanish.txt', 'Italian.txt']
+
+makingDictionaryOfWordsPerLanguage = {
+    'English': {'listEnglish' : [], countEnglish = 0}
+
+listFrench = []
+countFrench = 0
+
+listGerman = []
+countGerman = 0
+
+listSpanish = []
+countSpanish = 0
+
+listItalian = []
+countItalian = 0
+
+languageSources = ['sourceEnglish.txt', 'sourceFrench.txt', 'sourceGerman.txt', 'sourceSpanish.txt', 'sourceItalian.txt']
+
+# outer loop:
+for languageSource in languageSources: 
+    fileSource = open(languageSource,"r")
+    for lineSource in fileSource:
+        for wordSource in lineSource:
+            #if wordSource in lineSource:
                 # do something
-                pass
+                # pass
+
+            # (from last line): for every word in each line of the source text for a given language:
+            # (now, look at next bit of code): go through each word in all 5 language dolch word lists (really inefficient, I know):
+
+            # inner loop:
+            for languageDolch in languageDolchs:
+                fileDolch = open(languageDolch,"r")
+                for lineDolch in fileDolch:
+                    for wordDolch in lineDolch:
+
+                        # and finally,
+                        # if the current word of the current language Source text
+                        # is also present in the ____other_thing____variable____
+                        # of the dolch word list for the current language
+                        # is 
+
+                        if wordSource == wordDolch:
+                            # get lang
+                            currentLanguage=languageSource.slice[6:]
+                            
+                            # get both list and count
+                            # append to list
+                            # add to count
+                            pass
+
+# get word
+
+
+
+
