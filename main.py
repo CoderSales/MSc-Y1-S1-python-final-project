fdf06eba3d5b6bbe11efdf5948ab660b6bf6d260
Coder731 <stephenobrien1@gmail.com>
2023-11-22 01:07:32 +0000

    Bug Access dictionary so code runs
    Unexpected output
    letters
    output
    on a loop
    interrupted
    manually
    using
    ctrl C
    on Windows

--- main.py
+++ main.py
@@ -19,25 +19,37 @@
 - which ever is the highest count
 - this is the language detected.
 
+References:
+(0) VScode with Python Extension featuring Intellisense
+(1) slice() | W3Schools | https://www.w3schools.com/python/ref_func_slice.asp
+(2) Google Search string: | python pass contents of a variable name into a dictionary reference | https://www.google.com/search?q=python+pass+contents+of+a+variable+name+into+a+dictionary+reference&rlz=1C1YTUH_enIE1084IE1084&oq=python+pass+contents+of+a+variable+name+into+a+dictionary+reference&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRhA0gEJMzE3MzRqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8 
+(3) Result: | Use square brackets to access a dictionary key using a variable, e.g. my_dict[variable]. | https://bobbyhadz.com/blog/python-use-variable-to-access-dictionary-key#:~:text=Use%20square%20brackets%20to%20access,my_dict%5Bstr(variable)%5D%20.
+(4) Google Search string: | python method to find index position of a character in a string | https://www.google.com/search?q=python+method+to+find+index+position+of+a+character+in+a+string&rlz=1C1YTUH_enIE1084IE1084&oq=python+method+to+find+index+position+of+a+character+in+a+string&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRhA0gEJMjU1NzlqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8
+(5) Result: | Python String find() with Examples | https://sparkbyexamples.com/python/python-string-find-with-examples/#:~:text=find()%20method%20is%20used,character%20or%20String%20not%20found.
+(6) 3.11.6 Documentation » The Python Tutorial | https://docs.python.org/3.11/tutorial/index.html
+(7) dictionaries | https://docs.python.org/3.11/tutorial/datastructures.html#dictionaries
+
+
+
+
+Note:
+Using python --version | Python 3.11.6
 
 """
 
 languageDolchs = ['English.txt', 'French.txt', 'German.txt', 'Spanish.txt', 'Italian.txt']
 
 makingDictionaryOfWordsPerLanguage = {
-    'English': {'listEnglish' : [], countEnglish = 0}
+    'English': {'listEnglish' : [], 'countEnglish' : 0},
 
-listFrench = []
-countFrench = 0
+    'French':  {'listFrench' :  [], 'countFrench' : 0},
 
-listGerman = []
-countGerman = 0
+    'German' : {'listGerman' : [], 'countGerman' : 0},
 
-listSpanish = []
-countSpanish = 0
+    'Spanish' : {'listSpanish' : [], 'countSpanish' : 0},
 
-listItalian = []
-countItalian = 0
+    'Italian' : {'listItalian' : [], 'countItalian' : 0}
+}
 
 languageSources = ['sourceEnglish.txt', 'sourceFrench.txt', 'sourceGerman.txt', 'sourceSpanish.txt', 'sourceItalian.txt']
 
@@ -46,7 +58,7 @@ for languageSource in languageSources:
     fileSource = open(languageSource,"r")
     for lineSource in fileSource:
         for wordSource in lineSource:
-            #if wordSource in lineSource:
+            # if wordSource in lineSource:
                 # do something
                 # pass
 
@@ -67,15 +79,59 @@ for languageSource in languageSources:
 
                         if wordSource == wordDolch:
                             # get lang
-                            currentLanguage=languageSource.slice[6:]
+                            currentLanguage = languageSource[6:languageSource.find(".")] # English # hopefully type is String 
+                            # so if word is English then,
+                            # we get:
+                            # get both list:
+                            # current_list = makingDictionaryOfWordsPerLanguage[currentLanguage][0].listEnglish # [] to start off with
+                            # debugging above line:
+
+                            # now: do same for list: or reverse order
+                            language_key_for_list = 'list' + currentLanguage
+
+
+                            # now: construct variable name count + currentLanguage
+                            # for use as key for inner object in dictionary makingDictionaryOfWordsPerLanguage
+                            language_key_for_count = 'count' + currentLanguage
                             
-                            # get both list and count
-                            # append to list
-                            # add to count
-                            pass
 
-# get word
+                            current_list = makingDictionaryOfWordsPerLanguage[currentLanguage][language_key_for_list]
+                            print(current_list)
+                            # and count:
+                            current_count = makingDictionaryOfWordsPerLanguage[currentLanguage][language_key_for_count] # 0 to start off with
+                            # append to list:
+                            current_list.append(wordSource) # appendwhat?: append wordSource
+                            # add to count:
+                            current_count=current_count+1
 
+# Next do a count of which count is most:
+# So: 
+# max function on all 5:
+# So:
+# first go down to start to define max function
+# then come back up and define arguments a-e:
 
+# define variables that will be used as argumwntsto call max function:
+a = makingDictionaryOfWordsPerLanguage.English.countEnglish
 
+b = makingDictionaryOfWordsPerLanguage.French.countFrench
 
+c = makingDictionaryOfWordsPerLanguage.German.countGermnan
+
+d = makingDictionaryOfWordsPerLanguage.Spanish.countSpanish
+
+e = makingDictionaryOfWordsPerLanguage.Italian.countItalian
+
+languageOfArticle = maximum_checker(a,b,c,d,e)
+
+def maximum_checker(a,b,c,d,e):
+    maximum=a
+    if b>maximum:
+        maximum=b
+    if c>maximum:
+        maximum=c
+    if d>maximum:
+        maximum=d
+    if e>maximum:
+        maximum=e
+    return maximum
