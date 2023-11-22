1f948761f3f13215f9336d0409810ed6203faff4
Coder731 <stephenobrien1@gmail.com>
2023-11-22 23:40:56 +0000

    Cut commented template code
    from
    
    if wordDolch in Eng txt ret Eng 2 py

--- if-wordDolch-in-Eng-txt-ret-Eng-2.py
+++ if-wordDolch-in-Eng-txt-ret-Eng-2.py
@@ -1,8 +1,4 @@
 # .replace() | w3schools | txt = "I like bananas"; x = txt.replace("bananas", "apples"); print(x) | https://www.w3schools.com/python/ref_string_replace.asp
-
-# txt = "I like bananas"
-# x = txt.replace("bananas", "apples")
-# print(x)
 file_name_dolch="English.txt"
 wordDolch = 'the'
 file = open(file_name_dolch, 'r', encoding='utf8')
