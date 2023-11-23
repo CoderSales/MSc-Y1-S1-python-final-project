fb0e5f5ce3f2e12c11666ffa927780b204e701ed
Coder731 <stephenobrien1@gmail.com>
2023-11-23 05:08:14 +0000

    languagesDolchs py Cut comment on dict update

--- languagesDolchs.py
+++ languagesDolchs.py
@@ -4,8 +4,5 @@ def list_of_Dolchs():
     dolchs = ['English.txt', 'French.txt', 'German.txt', 'Spanish.txt', 'Italian.txt']
     for dolch_file_name in dolchs:
         opened = opener(dolch_file_name)
-        # dict_object append is for list
-        # how to with dict?
-        # update()
         dict_object_dolchs.update(opened)
     return dict_object_dolchs
