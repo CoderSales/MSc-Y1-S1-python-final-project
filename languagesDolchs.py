27a7cee67a54f861f96187197d89d6b1051d1da1
Coder731 <stephenobrien1@gmail.com>
2023-11-23 04:07:34 +0000

    Bug languagesDolchs py Try use map for list
    called
    languagesDolchs
    
    trying to
    package up
    encapsulate
    in a file
    the languageDolchs
    
    implementation
    uncertain
    
    not sure how to incorporate map
    or
    select correct file
    
    note
    consideration
    separation of concerns
    
    need to both iterate
    over
    words
    in languages
    both
    dolch
    and
    source text
    
    and
    
    be able to select
    
    either languages
    possibly
    
    or files possibly

---
+++ languagesDolchs.py
@@ -0,0 +1,7 @@
+def (1, language):
+    languageDolchs = ['English.txt', 'French.txt', 'German.txt', 'Spanish.txt', 'Italian.txt']
+    text_extension = '.txt'
+    if language == 'English'
+        file_name = language + text_extension
+        if language in languageDolchs:
+            return languageDolchs.charAt or indexOf
