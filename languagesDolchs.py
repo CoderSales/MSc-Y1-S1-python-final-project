303ea904b81cbeddc12656c59f6cdf7e88fe4654
Coder731 <stephenobrien1@gmail.com>
2023-11-23 04:43:56 +0000

    languagesDolchs py Add for loop

--- languagesDolchs.py
+++ languagesDolchs.py
@@ -1,6 +1,7 @@
 from open-holder import opener
 
 def list_of_Dolchs():
-    languageDolchs = ['English.txt', 'French.txt', 'German.txt', 'Spanish.txt', 'Italian.txt']
-    opened = opener()
-    return dict_with_file_objects_for_each_Dolch
+    dolchs = ['English.txt', 'French.txt', 'German.txt', 'Spanish.txt', 'Italian.txt']
+    for dolch_file_name in dolchs:
+        opened = opener(dolch_file_name)
+        return dict_with_file_objects_for_each_Dolch
