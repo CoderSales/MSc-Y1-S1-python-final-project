b092435f972139c5b4c2f90a62734df3ef60da49
Coder731 <stephenobrien1@gmail.com>
2023-11-23 05:16:24 +0000

    Try writing looper through Dolchs py file
    TODO
    write test driver function
    for
    looper through Dolchs py

---
+++ looper_through_Dolchs.py
@@ -0,0 +1,12 @@
+from languagesDolchs import list_of_Dolchs
+saved_list_of_dolchs = use list_of_Dolchs()
+
+# loop (del comment)
+if one_specified_language_dolch_object in saved_list_of_dolchs:
+    # hmmm ... how can we 
+    # not just always return the first one
+    # in the for loop?
+    # should this be an
+    # if statement instead?
+    # with a map?
+    return one_specified_language_dolch_object
