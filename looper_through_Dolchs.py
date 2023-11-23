05c46006b9733a8897e6262e8594e352b15783e7
Coder731 <stephenobrien1@gmail.com>
2023-11-23 05:21:09 +0000

    Update looper through Dolchs py
    so that
    
    the code
    
    is contained
    
    inside
    
    a function
    
    which can then
    
    be called from
    
    test driver for looper Dolchs py
    
    file
    
    TODO write the latter file
    
    - in progress

--- looper_through_Dolchs.py
+++ looper_through_Dolchs.py
@@ -1,12 +1,18 @@
 from languagesDolchs import list_of_Dolchs
-saved_list_of_dolchs = use list_of_Dolchs()
 
-# loop (del comment)
-if one_specified_language_dolch_object in saved_list_of_dolchs:
-    # hmmm ... how can we 
-    # not just always return the first one
-    # in the for loop?
-    # should this be an
-    # if statement instead?
-    # with a map?
-    return one_specified_language_dolch_object
+def looper_function_through_dolchs(one_specified_language_dolch_object):
+    """
+    should this parameter be a name of same?
+    """
+
+    saved_list_of_dolchs = use list_of_Dolchs()
+
+    # loop (del comment)
+    if one_specified_language_dolch_object in saved_list_of_dolchs:
+        # hmmm ... how can we 
+        # not just always return the first one
+        # in the for loop?
+        # should this be an
+        # if statement instead?
+        # with a map?
+        return one_specified_language_dolch_object
