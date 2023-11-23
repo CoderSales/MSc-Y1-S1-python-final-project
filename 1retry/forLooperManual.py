21d65a87f554f3195b2b99e9a3cce5db7b5b3126
Coder731 <stephenobrien1@gmail.com>
2023-11-23 09:22:47 +0000

    Add forLooperManual py
    try to implement
    
    for loop
    
    with manual incrementing
    
    and
    
    map
    
    and
    
    word handling

---
+++ 1retry/forLooperManual.py
@@ -0,0 +1,33 @@
+def make_counter():
+    count = -1
+    def _():
+        count += 1
+        return count
+    return _
+
+count_row = make_counter()
+
+
+##############
+
+def function(index, argument):
+
+    return x
+
+############
+
+for each word in the file
+it has an index 
+map it
+
+def incrementerByOne(count):
+    return count+1
+
+map(incrementerByOne(count), count):
+
+
+def indexHandler(indexHandling):
+    return indexHandling +1
+
+def wordHandler(word):
+    return word
