39b20853e260a386cbf3ed0779cc81600c786886
Coder731 <stephenobrien1@gmail.com>
2023-11-23 05:25:20 +0000

    Try setting one specified language dolch object
    to
    the string
    English
    
    for
    file
    
    test driver for looper Dolchs py

---
+++ test_driver_for_looper_Dolchs.py
@@ -0,0 +1,4 @@
+from looper_through_Dolchs import looper_through_Dolchs
+one_specified_language_dolch_object='English'
+returned_object = looper_through_dolchs(one_specified_language_dolch_object)
+print(returned_object)
