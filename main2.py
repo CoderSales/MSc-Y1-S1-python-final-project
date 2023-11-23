cd184d070a94adbd72653df78cc2dd5b99657183
Coder731 <stephenobrien1@gmail.com>
2023-11-23 09:59:49 +0000

    main2 py add max checker step

--- main2.py
+++ main2.py
@@ -26,36 +26,35 @@ countItalian = 0
 
 
 # outer loop:
-for languageSource in languageSources: 
-    fileSource = open(languageSource,"r", encoding="utf8")
-    for word_is_line_Source in fileSource:
-        list_of_individual_words = word_is_line_Source.split(' ') 
+fileSource = open(languageSource,"r", encoding="utf8")
+for word_is_line_Source in fileSource:
+    list_of_individual_words = word_is_line_Source.split(' ') 
 
-        for wordSource in list_of_individual_words:
+    for wordSource in list_of_individual_words:
 
-            # inner loop:
-            for languageDolch in languageDolchs:
-                fileDolch = open(languageDolch,"r")
-                for word_per_line_Dolch in fileDolch:
-                    # print(word_per_line_Dolch) # comment out word print as too many # working print 1
-                    print(word_per_line_Dolch) # print 4 working # prints words
-                    if word_is_line_Source == word_per_line_Dolch:
-                        currentLanguage = languageSource[6:languageSource.find(".")]
-                        language_key_for_list = 'list' + currentLanguage
-                        language_key_for_count = 'count' + currentLanguage
-                        current_list = language_key_for_list
+        # inner loop:
+        for languageDolch in languageDolchs:
+            fileDolch = open(languageDolch,"r")
+            for word_per_line_Dolch in fileDolch:
+                # print(word_per_line_Dolch) # comment out word print as too many # working print 1
+                print(word_per_line_Dolch) # print 4 working # prints words
+                if word_is_line_Source == word_per_line_Dolch:
+                    currentLanguage = languageSource[6:languageSource.find(".")]
+                    language_key_for_list = 'list' + currentLanguage
+                    language_key_for_count = 'count' + currentLanguage
+                    current_list = language_key_for_list
 
 
 # count
 
 
-                        current_count = language_key_for_count # 0 to start off with
-                        
-                        # add to count:
-                        current_count=current_count+1
+                    current_count = language_key_for_count # 0 to start off with
+                    
+                    # add to count:
+                    current_count=current_count+1
 
 
-# 
+# max checker
 
 def maximum_checker(countEnglish,countFrench,countGerman,countSpanish,countItalian):
     maximum=countEnglish
