3d8e44230c4a03eea03eebcfb89cb2124435de7b
Coder731 <stephenobrien1@gmail.com>
2023-11-23 03:22:18 +0000

    Use file2 py for path

--- file1.py
+++ file1.py
@@ -7,5 +7,5 @@ import pandas as pd
 #%% load file via static path
   
 def load_df():
-    tester=r'C:\user\steph\OneDrive\Documents\45-semester-1\module-language-engineering\10-week-10-on\1-py-final-proj\MSc-Y1-S1-python-final-project\file2.py'
+    tester=r'file2.py'
     return pd.read_csv(tester, sep=';',decimal=",", skipfooter=1300, engine='python')
